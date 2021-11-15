142517abb5842a5d6acc2f35e61cf9f6cb8892ec
George Taylor <georgemauricetaylor@gmail.com>
2021-11-15 09:13:32 +0000

    reformatting code

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -2,26 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 const multer = require('multer');
-const upload = multer({ dest: 'tmp/csv/' });
+const upload = multer({dest: 'tmp/csv/'});
 var csv = require('csvtojson');
 
 const parseHomeOwnerService = require('../libs/services/parseHomeOwnersService');
 
 router.get('/example', async function (req, res) {
 
-        csv({ignoreEmpty: true}).fromFile('./example.csv').on('error',()=>{
-            res.status(500).end();
-        }).then((json) => {
-            const result = parseHomeOwnerService.parseNames(json);
+    csv({ignoreEmpty: true}).fromFile('./example.csv').on('error', () => {
+        res.status(500).end();
+    }).then((json) => {
+        const result = parseHomeOwnerService.parseNames(json);
 
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(result));
-        });
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));
+    });
 
 });
 
-router.post('/',upload.single('file'), async function (req, res) {
-    csv({ignoreEmpty:true}).fromFile(req.file.path).on('error',()=>{
+router.post('/', upload.single('file'), async function (req, res) {
+    csv({ignoreEmpty: true}).fromFile(req.file.path).on('error', () => {
         res.status(500).end();
     }).then((json) => {
         const result = parseHomeOwnerService.parseNames(json);
